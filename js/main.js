1a9c3840042b1b76177141437624462e19197c76
Rafaelac878 <130992685+Rafaelac878@users.noreply.github.com>
2023-08-20 18:05:58 -0300

    portifolio_projeto_one_desafio
    up site

---
+++ js/main.js
@@ -0,0 +1,32 @@
+const backgroundElement = document.getElementById('header__menu');
+const textElements = document.getElementsByClassName('banner__list__link');
+const menuElements = document.getElementsByClassName('open__menu__link');
+
+// Validação do formulário
+function validarFormulario() {
+  var name = document.getElementById('name').value;
+  var email = document.getElementById('email').value;
+  var topic = document.getElementById('topic').value;
+  var message = document.getElementById('textArea').value;
+
+  // Validar se os campos estão preenchidos
+  if (name.trim() === '' || email.trim() === '' || topic.trim() === '' || message.trim() === '') {
+      alert('Por favor, preencha todos os campos.');
+      return false;
+  }
+
+  // Validar o formato do e-mail
+  var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!emailRegex.test(email)) {
+      alert('Por favor, insira um endereço de e-mail válido.');
+      return false;
+  }
+  
+  // Se todas as validações passarem, o formulário é enviado
+  alert('Formulário enviado com sucesso!');
+
+   // Limpar os valores dos campos
+   document.getElementById('form').reset();
+
+  return true;
+}
